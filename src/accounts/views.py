0bd6f433a2cba9daa35b6c2d7855ef1f9b12b043
rgbosque <rgbosque@gmail.com>
2018-09-19 07:40:46 +0800

    create login modules

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,19 @@
+from django.contrib.auth import get_user_model, login
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import UserLoginForm
+
+User = get_user_model()
+
+
+def user_login(request, *args, **kwargs):
+    form = UserLoginForm(request.POST or None)
+    if form.is_valid():
+        uname_ = form.cleaned_data.get('username')
+        user_obj = User.objects.get(username__iexact=uname_)
+        login(request, user_obj)
+        return HttpResponseRedirect('/dashboard')
+
+    context = {"form": form}
+    return render(request, 'accounts/login.html', context)
