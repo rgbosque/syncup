f37cc4cd5282ed997ae9185a94deb8c8bcf77e5c
rgbosque <rgbosque@gmail.com>
2018-09-19 12:55:25 +0800

    create register modules and add css/img/js static files

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model, login, logout
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-from .forms import UserLoginForm
+from .forms import UserLoginForm, UserCreationForm
 
 User = get_user_model()
 
@@ -15,10 +15,18 @@ def user_login(request, *args, **kwargs):
         login(request, user_obj)
         return HttpResponseRedirect('/dashboard')
 
-    context = {"form": form}
-    return render(request, 'accounts/login.html', context)
+    return render(request, 'accounts/login.html', {'form': form})
 
 
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect('/accounts/login')
+
+
+def register(request, *args, **kwargs):
+    form = UserCreationForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect('accounts/login')
+
+    return render(request, 'accounts/register.html', {'form': form})
