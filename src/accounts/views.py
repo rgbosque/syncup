d8bed68f0bd63eeea1b7085d6ac0c29767b3a900
rgbosque <rgbosque@gmail.com>
2018-09-21 18:21:00 +0800

    Update login navbar menu

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -13,14 +13,14 @@ def user_login(request, *args, **kwargs):
         uname_ = form.cleaned_data.get('username')
         user_obj = User.objects.get(username__iexact=uname_)
         login(request, user_obj)
-        return HttpResponseRedirect('/dashboard')
+        return HttpResponseRedirect('/')
 
     return render(request, 'accounts/login.html', {'form': form})
 
 
 def user_logout(request):
     logout(request)
-    return HttpResponseRedirect('/accounts/login')
+    return HttpResponseRedirect('/')
 
 
 def register(request, *args, **kwargs):
